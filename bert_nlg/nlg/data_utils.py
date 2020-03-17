97883402497bafb9ce890135aa38469ec848ac24
KnightZhang <knight.625@live.com>
2020-03-17 22:27:57 +0800

    fix padding in each batch problem

--- bert_nlg/nlg/data_utils.py
+++ bert_nlg/nlg/data_utils.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
 import codecs
+import random
 import numpy as np
 import tensorflow as tf
 from functools import partial
@@ -9,6 +10,7 @@ tf.enable_eager_execution()
 import config as cg
 
 def train_generator(path):
+  """this could achieve random by dataset.shuffle(10).batch(2).repeat(3)."""
   with codecs.open(path, 'r', 'utf-8') as file:
     for line in file:
       if len(line) != 0:
@@ -18,17 +20,39 @@ def train_generator(path):
         features = {'input': line}
         yield features
 
+str_to_int = lambda string : list(map(int, string))
+def train_generator_test(path):
+  """this could achieve padding among each batch."""
+  with codecs.open(path, 'r', 'utf-8') as file:
+    data = file.read().split('\n')[:-1]
+  data = list(map(str_to_int, data))
+  
+  random.shuffle(data)
+  lines = []
+  for idx, line in enumerate(data):    
+    if len(lines) < 2:
+      lines.append(line)
+      if len(lines) == 2:
+        features = {'input': lines}
+        yield features
+        lines = []
+      if idx > (len(data) // 2) * 2 - 1 and len(data) % 2 != 0:
+        lines.extend(random.sample(data, 2 - len(lines)))
+        features = {'input': lines}
+        yield features
+        lines = []
+
 def train_input_fn():
   output_types = {'input': tf.int32}
-  output_shapes = {'input': [None]}
+  output_shapes = {'input': [None, None]}
 
-  generator = partial(train_generator, path=cg.DATA_PATH)
+  generator = partial(train_generator_test, path=cg.DATA_PATH)
   dataset = tf.data.Dataset.from_generator(
     generator,
     output_types=output_types,
     output_shapes=output_shapes)
 
-  dataset = dataset.shuffle(10).batch(2).repeat(3)
+  dataset = dataset.repeat(3)
 
   return dataset
 
@@ -39,3 +63,7 @@ if __name__ == '__main__':
   
   # for data in train_generator(cg.DATA_PATH):
   #   print(data)
+  
+  # for data in train_generator_test(cg.DATA_PATH):
+  #   print(data)
+  #   input()
